e571bc946524fc5768bc89d8df50ddefaabdb459
Matt Cooper <matt.cooper.85@gmail.com>
2022-07-15 18:43:26 +1000

    Clean-up code

--- javascript.js
+++ javascript.js
@@ -1,47 +1,48 @@
 function computerPlay() {
     randoNum = Math.floor(Math.random() * 3) +1;
     if(randoNum == 1) {
-        return "paper"
+        return "paper";
     } else if (randoNum == 2) {
-        return "scissors"
+        return "scissors";
     } else {
-        return "rock"
+        return "rock";
     }
 }
 
 function playerPlay() {
-    let hand = prompt("Choose rock, paper or scissors");
-    console.log(hand);
-    if (hand.toLowerCase() == "rock" || hand.toLowerCase() == "paper" || hand.toLowerCase() == "scissors") {
+    let hand = prompt("Choose paper, scissors or rock.").toLowerCase();
+    /* let hand = userResponse.toLowerCase(); */
+    if (hand == "rock" || hand == "paper" || hand == "scissors") {
+        console.log(`Player chooses %s`, hand);
         return hand;
     } else {
-        console.log("Did not choose correctly");
+        let hand = ""
+        console.log("You must choose paper, scissors or rock.");
         playerPlay();
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    /* playerSelection = playerSelection.toLowerCase() */
     if (playerSelection == computerSelection) {
-        return 0       
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("Player wins this round - Rock smashes scissors")
-        return 1
+        return 0;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("Player wins this round - Paper wraps rock")
-        return 1
+        console.log("Computer chooses rock - Paper wraps rock - Player wins this round.");
+        return 1;
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("Player wins this round - Scissors slices paper")
-        return 1
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log("Computer wins this round - Rock smashes scissors")
-        return 2
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("Computer wins this round - Paper wraps rock")
-        return 2
+        console.log("Computer chooses paper - Scissors slice paper - Player wins this round.");
+        return 1;
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("Computer chooses scissors - Rock smashes scissors - Player wins this round.")
+        return 1;
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log("Computer wins this round - Scissors slices paper")
-        return 2
+        console.log("Computer chooses scissors  - Scissors slice paper - Computer wins this round.");
+        return 2;
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        console.log("Computer chooses rock - Rock smashes scissors - Computer wins this round.");
+        return 2;
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log("Computer chooses paper - Paper wraps rock - Computer wins this round.");
+        return 2;
   }
 }
 
@@ -54,23 +55,22 @@ function game() {
         let result = playRound(playerSelection, computerSelection);
         if  (result == 2) {
             compScore++;
-            console.log("Point added to Computer's tally. Computer %s, Player %s", compScore, playerScore)
+            console.log("Point added to Computer's tally. Computer %s, Player %s", compScore, playerScore);
         } else if (result == 1) {
             playerScore++;
-            console.log("Point added to Players's tally. Computer %s, Player %s", compScore, playerScore)
+            console.log("Point added to Players's tally. Computer %s, Player %s", compScore, playerScore);
             } else {
-                console.log("Draw, no points awarded.")
+                console.log("Draw, no points awarded.");
             } 
         }
         console.log(`Player Score: %s. Computer Score: %s.`, playerScore, compScore);
         if (playerScore > compScore) {
-            console.log("Player wins the game!") 
+            console.log("Player wins the game!");
         } else if (playerScore < compScore) {
+            console.log("Computer wins the game!");
         } else {
-            console.log("It's a draw!")
+            console.log("It's a draw!");
         }
     }
 
-const playerSelection = "";
-const computerSelection = "";
 game();
